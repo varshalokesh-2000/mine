324ba8c0af76fc02533157d1ed83168a037b9b37
VarshaML <varsha.ml@tecnotree.com>
2025-02-08 00:38:54 +0530

    fix: one last time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import dynamic from "next/dynamic";
-const DoveLetterLoader = dynamic(() => import("@/components/DoveLetterLoader"), { ssr: false });
 import { useState } from "react";
 import AnimatedTextSequence from "@/components/AnimatedTextSequence";
-import GiftBox from "@/components/GiftBox";
+
+// Disable SSR for these components
+const DoveLetterLoader = dynamic(() => import("@/components/DoveLetterLoader"), { ssr: false });
+const GiftBox = dynamic(() => import("@/components/GiftBox"), { ssr: false });
 
 const MainComponent = () => {
   const [stage, setStage] = useState("home");
