a10e8be0dbf2ee81b589ce39844e1673ecfa1f0a
VarshaML <varsha.ml@tecnotree.com>
2025-02-08 00:40:20 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false }; // Avoid server-side issues
+    return config;
+  },
 };
 
 export default nextConfig;
